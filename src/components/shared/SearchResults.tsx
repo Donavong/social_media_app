17a5d99b69d2fcadc2336805e7b9b002145e2389
Donavong <dgreene1996@gmail.com>
2023-12-15 01:16:09 -0500

    finishing changes

---
+++ src/components/shared/SearchResults.tsx
@@ -0,0 +1,18 @@
+/*import { SearchResultProps } from '@/_root/pages/Explore'
+import React from 'react'
+import {Models} from 'appwrite';
+
+type SearchResultsProps={
+    isSearchFetching:boolean;
+    searchedPosts:Models.Document[];
+}
+
+const SearchResults = ({isSearchFetching, searchedPosts}
+    :SearchResultsProps) => {
+  return (
+    <div>SearchResults</div>
+  )
+}
+
+export default SearchResults
+*/
